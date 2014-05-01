30c1fe9228c973756e61bb672cad126672c2eeaa
Ovidijus <ovidijus.na@gmail.com>
2014-05-01 23:08:25 +0300

    add toDigits

--- projectEuler2/src/main/java/lt/oworks/projecteuler2/utils/MathUtils.java
+++ projectEuler2/src/main/java/lt/oworks/projecteuler2/utils/MathUtils.java
@@ -1,5 +1,8 @@
 package lt.oworks.projecteuler2.utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Ovidijus N.
@@ -21,6 +24,21 @@ public class MathUtils {
             return true;
         } else {
             return false;
+        }
+    }
+
+    public static List<Integer> toDigits(final long pNumber) {
+        if (pNumber < 0) {
+            return null;
+        } else {
+            final List<Integer> digits = new ArrayList<>();
+            long temp = pNumber;
+            do {
+                long lastDigit = temp % 10;
+                temp = temp / 10;
+                digits.add(0, (int) lastDigit);
+            } while (temp > 0);
+            return digits;
         }
     }
 }
--- projectEuler2/src/test/java/lt/oworks/projecteuler2/utils/MathUtilsTest.java
+++ projectEuler2/src/test/java/lt/oworks/projecteuler2/utils/MathUtilsTest.java
@@ -1,5 +1,6 @@
 package lt.oworks.projecteuler2.utils;
 
+import java.util.Arrays;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -23,6 +24,20 @@ public class MathUtilsTest {
         assertEquals("3", true, MathUtils.isPrime(3));
 
         assertEquals("4", false, MathUtils.isPrime(4));
+    }
+
+    @Test
+    public void testToDigits() {
+        assertNull(MathUtils.toDigits(-1));
+
+        assertEquals(Arrays.asList(new Integer[]{0}), MathUtils.toDigits(0));
+
+        assertEquals(Arrays.asList(new Integer[]{1}), MathUtils.toDigits(1));
+
+        assertEquals(Arrays.asList(new Integer[]{1, 0}), MathUtils.toDigits(10));
+
+        assertEquals(Arrays.asList(new Integer[]{1, 2, 3, 4, 5}), MathUtils.toDigits(12345));
+
     }
 
 }
